73e4649be2e6fb9dc964ff95a71a84e2a71ca95c
Poupart Valentin <poupart@b11p19.fil.univ-lille1.fr>
2018-09-21 14:57:31 +0200

    Push d'une erreur pour voir sur Travis

--- src/main/java/donjon/item/Bandit.java
+++ src/main/java/donjon/item/Bandit.java
@@ -5,7 +5,7 @@ import donjon.personnage.Player;
 import java.util.List;
 import java.util.Random;
 
-public class Bandit implements Item{
+public class Bandit {
     private int value;
     private List<Item> items;
 
--- src/main/java/donjon/main.java
+++ src/main/java/donjon/main.java
@@ -8,7 +8,7 @@ import donjon.room.*;
 
 public class main {
     public static void main (String[] args) {
-        Player player = ;
+        Player player = new Player();
         Room start = new Room();
 
         AdventureGame the_game = new AdventureGame(start, player);
